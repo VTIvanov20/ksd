1b2a05dc1b598d98ba9fa1ad3c833c76d31e4851
AEManov20 <AEManov20@codingburgas.bg>
2022-05-10 11:29:41 +0300

    Enable Anti-Aliasing flag in raylib

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main(int, char **)
 {
     SetConfigFlags(FLAG_VSYNC_HINT);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
 
     GameManager* gameManager = GameManager::GetInstance();
     gameManager->ChangeScene("res/scenes/main_menu.json");
